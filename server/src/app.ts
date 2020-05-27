af44dd8e9168eeef9bd22e3ed0c71a3c5ca83199
Brandon Blaschke <brandonj.blaschke@gmail.com>
2020-05-27 14:39:34 -0700

    Added client side to server side

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import userRouter from './routers/user_router';
 import exerciseRouter from './routers/exerciseRouter';
 import mongoose from 'mongoose';
+import path from 'path';
 
 const dbURL = process.env.MONGODB_URL || "";
 
@@ -18,9 +19,10 @@ app.use(express.json())
 // Add routers to project 
 app.use(userRouter);
 app.use(exerciseRouter);
+app.use(express.static(path.join(__dirname, "../..", "client/build")));
 
-app.get("/", function (req: any, res: any) {
-    res.send("Hello World!");
+app.get("/test", function (req: any, res: any) {
+    res.send("Hello World!2");
 });
 
 export default app;
