73543c057d91cc07aff8667d55a1fb4921ca89b5
Brandon Blaschke <brandonj.blaschke@gmail.com>
2020-05-27 18:16:50 -0700

    Start of login working

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import './css/login.css'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+  this.state = {
+    email: '',
+    password: ''
+  }
+  }
+
+
+  handleInput = (event) => {
+    const target = event.target;
+    if (target.name === "email") {
+      this.setState({email: target.value});
+    } else {
+      this.setState({password: target.value});
+    }
+  }
+
+  submit = (event) => {
+    event.preventDefault();
+    console.log(this.state.email, this.state.password);
+    axios.post('/user', {
+      name: "Name1",
+      email: this.state.email,
+      password: this.state.password
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((e) => {
+      console.log(e);
+    })
+  }
+
+  render() {
+    return (
+      <div className="login-page">
+        <div className="form">
+  
+          <form className="login-form">
+            <input onChange={this.handleInput} name="email" type="text" placeholder="username"/>
+            <input onChange={this.handleInput} name="password" type="password" placeholder="password"/>
+  
+            <button onClick={this.submit}>login</button>
+            <button onClick={this.submit}>Sign Up</button>
+            <p className="message">Not registered? <a href="#">Create an account</a></p>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
